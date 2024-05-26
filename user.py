cf651068e2740eba9c91d7a6d2e44a6da013c4bd
Vic-Hatem <hatemkhater0@gmail.com>
2024-05-26 14:39:57 +0300

    modified the user class

--- user.py
+++ user.py
@@ -1,3 +1,5 @@
+import json
+
 from pydantic import BaseModel, Field, EmailStr
 
 
@@ -6,6 +8,14 @@ class User(BaseModel):
     email: str
     password: str
 
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'email': self.email,
+            'password': self.password,
+        }
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=3)
 
 class AdminUser(User):
     def user_type(self):
